8e98e6c268b592898c5e53700349732c6de4b997
Santiagoacevedo5 <santiago.acevedoc1@udea.edu.co>
2026-09-15 00:54:38 -0500

    backendhu1
    s

---
+++ src/main/java/com/ey/springboot3security/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.ey.springboot3security.controller;
+
+import com.ey.springboot3security.entity.AuthRequest;
+import com.ey.springboot3security.entity.UserInfo;
+import com.ey.springboot3security.service.JwtService;
+import com.ey.springboot3security.service.UserInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    private final UserInfoService service;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    // Removed the role checks here as they are already managed in SecurityConfig
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
+    }
+}
